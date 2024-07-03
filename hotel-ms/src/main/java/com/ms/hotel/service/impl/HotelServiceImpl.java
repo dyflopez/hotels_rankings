d36aaa35cf659baa2341278a967214de26963b26
daniel florez <daniel0223@hotmail.es>
2024-07-02 19:02:49 -0500

    creacion micro usuarios

--- hotel-ms/src/main/java/com/ms/hotel/service/impl/HotelServiceImpl.java
+++ hotel-ms/src/main/java/com/ms/hotel/service/impl/HotelServiceImpl.java
@@ -5,10 +5,13 @@ import com.ms.hotel.model.HotelEntity;
 import com.ms.hotel.repository.IHotelRepository;
 import com.ms.hotel.service.IHotelService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -19,22 +22,46 @@ public class HotelServiceImpl implements IHotelService {
 
     @Override
     public ResponseEntity<List<HotelEntity>> getAll() {
-        return null;
+        List<HotelEntity> hotels = this.iHotelRepository.findAll();
+        return ResponseEntity
+                .status(HttpStatus.FOUND)
+                .body(hotels);
     }
 
     @Override
     public ResponseEntity<HotelEntity> create(HotelDto hotelDTO) {
-        return null;
+
+        HotelEntity hotel = HotelEntity
+                .builder()
+                .name(hotelDTO.getName())
+                .information(hotelDTO.getInformation())
+                .location(hotelDTO.getLocation())
+                .build();
+
+        HotelEntity newHotel = this.iHotelRepository.save(hotel);
+
+
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(newHotel);
     }
 
     @Override
     public ResponseEntity<HotelEntity> getById(UUID id) {
-        return null;
+
+        Optional<HotelEntity> hotelOp = this.iHotelRepository.findById(id);
+
+        if(hotelOp.isPresent()){
+            return ResponseEntity.status(HttpStatus.OK).body(hotelOp.get());
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new HotelEntity());
     }
 
     @Override
     public void deleteById(UUID id) {
-
+        this.iHotelRepository.deleteById(id);
     }
 
     @Override
